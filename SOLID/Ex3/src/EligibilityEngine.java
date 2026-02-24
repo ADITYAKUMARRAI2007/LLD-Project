f953a7b2a73bb10fa8819372f452b0c05ff6c1fa
ADITYAKUMARRAI2007 <adityarai04012007@gmail.com>
2026-02-24 05:45:04 +0530

    final class Eligibility

--- SOLID/Ex3/src/EligibilityEngine.java
+++ SOLID/Ex3/src/EligibilityEngine.java
@@ -39,12 +39,3 @@ public class EligibilityEngine {
         );
     }
 }
-
-class EligibilityEngineResult {
-    public final String status;
-    public final List<String> reasons;
-    public EligibilityEngineResult(String status, List<String> reasons) {
-        this.status = status;
-        this.reasons = reasons;
-    }
-}
